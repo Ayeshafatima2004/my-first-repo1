45e3466e4823918b68c4e85e41fb81968bf849f2
Ayeshafatima2004 <af6617020@gmail.com>
2024-11-29 11:34:01 +0500

    i have aded a chart as first feature

---
+++ feature1.py
@@ -0,0 +1,9 @@
+# sample feature chart
+import matplotlib.pyplot as plt
+
+names = ['banana','apple','oranges','mango']
+units = [20,30,45,55]
+
+plt.bar(names,units)
+plt.title("Chart for sales of fruits ")
+plt.show()
